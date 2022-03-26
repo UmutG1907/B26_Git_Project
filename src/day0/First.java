e43f70232768a939b87dd554c2b64180e4fdc2e0
umut <umutcan_gundogdu1@hotmail.com>
2022-03-26 13:10:48 -0700

    Wrote the code for the first class

--- src/day0/First.java
+++ src/day0/First.java
@@ -3,12 +3,11 @@ package day0;
 public class First {
     public static void main(String[] args) {
 
-        //TODO: Work on this tomorrow
-        // add some cool code
-
-        // extra line 1
-        // extra line 2
-        // extra line 3
+        System.out.println("Java");
+        System.out.println("Working..");
 
     }
 }
+        // extra line 1
+        // extra line 2
+        // extra line 3
